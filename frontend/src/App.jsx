622343177b0fdcfbc35d26d6dbe2d787e78f3346
elsagashi1 <elsagashhi333@gmail.com>
2026-04-23 11:40:09 +0200

    wishlist&routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
 import ShoppingCartPage from "./pages/ShoppingCartPage";
+import WishlistPage from "./pages/WishlistPage";
 
 function App() {
   return (
-    <>
-      <ShoppingCartPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<ShoppingCartPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+      </Routes>
+    </Router>
   );
 }
 
