7686556d3f684cd7aebf3a44d91e3fefa155ea69
ErmalShabani1 <ermal_shabani05@hotmail.com>
2026-04-25 22:58:57 +0200

    -Rregullimi i routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,15 @@ import HomePage from "./pages/HomePage";
 import ShoppingCartPage from "./pages/ShoppingCartPage";
 import WishlistPage from "./pages/WishlistPage";
 import CheckoutPage from "./pages/CheckoutPage";
+import CustomerPage from "./pages/CustomerPage";
+import FAQsPage from "./pages/FAQsPage";
+import SignInPage from "./pages/SignInPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import SignUpPage from "./pages/SignUpPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import AboutUsPage from "./pages/AboutUsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -12,6 +21,16 @@ function App() {
         <Route path="/cart" element={<ShoppingCartPage />} />
         <Route path="/wishlist" element={<WishlistPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/customer" element={<CustomerPage />} />
+        <Route path="/faqs" element={<FAQsPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/email-verification" element={<EmailVerificationPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
