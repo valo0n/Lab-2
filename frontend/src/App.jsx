5f10137b93271e2b881e0040d162905327cc05f1
valo0n <valonnura888@outlook.com>
2026-05-07 15:14:26 +0200

    rregullova butonat e CartPopup dhe ProductCard - checkout, delete, add to cart, quick view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,47 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
 import ShoppingCartPage from "./pages/ShoppingCartPage";
 import WishlistPage from "./pages/WishlistPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import CustomerPage from "./pages/CustomerPage";
+import DashboardPage from "./pages/DashboardPage";
 import FAQsPage from "./pages/FAQsPage";
+import BlogPage from "./pages/BlogPage";
+import AboutUsPage from "./pages/AboutUsPage";
 import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
-import SignUpPage from "./pages/SignUpPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
-import AboutUsPage from "./pages/AboutUsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Faqet kryesore */}
         <Route path="/" element={<HomePage />} />
+        <Route path="/product/:slug" element={<ProductDetailsPage />} />
         <Route path="/cart" element={<ShoppingCartPage />} />
         <Route path="/wishlist" element={<WishlistPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+
+        {/* Llogaria */}
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/customer" element={<CustomerPage />} />
         <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/email-verification" element={<EmailVerificationPage />} />
+
+        {/* Info faqet */}
+        <Route path="/blog" element={<BlogPage />} />
         <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/faqs" element={<FAQsPage />} />
+
+        {/* 404 */}
         <Route path="/404" element={<NotFoundPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
