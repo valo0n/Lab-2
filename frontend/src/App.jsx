2d50ef17d3f70fbab88cacc0ffcd6d631aca18d9
valo0n <valonnura888@outlook.com>
2026-05-12 12:51:08 +0200

    perditesova importet ne App.jsx sipas strukturës se re te folderave

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,43 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { WishlistProvider } from "./context/WishlistContext";
-import HomePage from "./pages/HomePage";
-import ShoppingCartPage from "./pages/ShoppingCartPage";
-import WishlistPage from "./pages/WishlistPage";
-import CheckoutPage from "./pages/CheckoutPage";
-import CustomerPage from "./pages/CustomerPage";
-import FAQsPage from "./pages/FAQsPage";
-import SignInPage from "./pages/SignInPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
-import SignUpPage from "./pages/SignUpPage";
-import EmailVerificationPage from "./pages/EmailVerificationPage";
-import AboutUsPage from "./pages/AboutUsPage";
+
+// Home
+import HomePage from "./pages/home/HomePage";
+
+// Shop
+import ShopPage from "./pages/shop/ShopPage";
+import ProductDetailsPage from "./pages/shop/ProductDetailsPage";
+import ComparePage from "./pages/shop/ComparePage";
+
+// Cart & Checkout
+import ShoppingCartPage from "./pages/cart/ShoppingCartPage";
+import CheckoutPage from "./pages/cart/CheckoutPage";
+
+// Auth
+import SignInPage from "./pages/auth/SignInPage";
+import SignUpPage from "./pages/auth/SignUpPage";
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
+import EmailVerificationPage from "./pages/auth/EmailVerificationPage";
+
+// Account
+import DashboardPage from "./pages/account/DashboardPage";
+import CustomerPage from "./pages/account/CustomerPage";
+import WishlistPage from "./pages/account/WishlistPage";
+import TrackOrderPage from "./pages/account/TrackOrderPage";
+import TrackOrderDetails from "./pages/account/TrackOrderDetails";
+
+// Info
+import AboutUsPage from "./pages/info/AboutUsPage";
+import BlogPage from "./pages/info/BlogPage";
+import FAQsPage from "./pages/info/FAQsPage";
+
+// Order
+import SuccessPage from "./pages/order/SuccessPage";
+
+// 404
 import NotFoundPage from "./pages/NotFoundPage";
-import DashboardPage from "./pages/DashboardPage";
-import SuccessPage from "./pages/SuccessPage";
-import TrackOrderDetails from "./pages/TrackOrderDetails";
-import TrackOrderPage from "./pages/TrackOrderPage";
-import ShopPage from "./pages/ShopPage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
-import ComparePage from "./pages/ComparePage";
-import BlogPage from "./pages/BlogPage";
 
 function App() {
   return (
@@ -29,32 +45,47 @@ function App() {
       <WishlistProvider>
         <Router>
           <Routes>
+            {/* Home */}
             <Route path="/" element={<HomePage />} />
-            <Route path="/cart" element={<ShoppingCartPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/wishlist" element={<WishlistPage />} />
-            <Route path="/compare" element={<ComparePage />} />
+
+            {/* Shop */}
             <Route path="/shop-page" element={<ShopPage />} />
             <Route path="/product-details" element={<ProductDetailsPage />} />
-            <Route path="/success" element={<SuccessPage />} />
+            <Route path="/compare" element={<ComparePage />} />
+
+            {/* Cart & Checkout */}
+            <Route path="/cart" element={<ShoppingCartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+
+            {/* Auth */}
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} />
+            <Route
+              path="/email-verification"
+              element={<EmailVerificationPage />}
+            />
+
+            {/* Account */}
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/customer" element={<CustomerPage />} />
+            <Route path="/wishlist" element={<WishlistPage />} />
             <Route path="/track-order" element={<TrackOrderPage />} />
             <Route
               path="/track-order-details"
               element={<TrackOrderDetails />}
             />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/customer" element={<CustomerPage />} />
-            <Route path="/signin" element={<SignInPage />} />
-            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-            <Route path="/reset-password" element={<ResetPasswordPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route
-              path="/email-verification"
-              element={<EmailVerificationPage />}
-            />
+
+            {/* Info */}
             <Route path="/about-us" element={<AboutUsPage />} />
             <Route path="/blog" element={<BlogPage />} />
             <Route path="/faqs" element={<FAQsPage />} />
+
+            {/* Order */}
+            <Route path="/success" element={<SuccessPage />} />
+
+            {/* 404 */}
             <Route path="/404" element={<NotFoundPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
